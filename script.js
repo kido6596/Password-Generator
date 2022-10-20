d94d049a49bab64ff7fdd47fdfbe7a03eee1c3ed
sarah 3 <109850449+kido6596@users.noreply.github.com>
2022-10-20 12:07:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-const lengthSlider = document.querySelector('.pass-length input');
+const lengthSlider = document.querySelector(".pass-length input"),
+options = document.querySelectorAll(".option input"),
+generateBtn = document.querySelector(".generate-btn");
+
+const characters = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVXWYZ",
+    numbers: "0123456789",
+    symbols: "^!$%&|[](){}:;.,*+-#@<>~"
+}
+
+const generatePassword = () => {
+    let staticPassword = "";
+
+    options.forEach(option => {
+        if(option.checked) {
+            staticPassword += characters[option.id];
+        }
+    });
+
+    console.log(staticPassword);
+}
 
 const updateSlider = () => {
     document.querySelector('.pass-length span').innerText = lengthSlider.value;
 }
+updateSlider();
 
 lengthSlider.addEventListener('input', updateSlider);
+generateBtn.addEventListener('click', generatePassword);
